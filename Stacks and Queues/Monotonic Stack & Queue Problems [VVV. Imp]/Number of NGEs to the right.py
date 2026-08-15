d6f80e0fa8da29cfc1c41b56c0fe83e79109446a
ananthjeethvuppala <ananthjeeth@gmail.com>
2026-08-15 23:06:46 +0530

    15-08-2026(No progress)

---
+++ Stacks and Queues/Monotonic Stack & Queue Problems [VVV. Imp]/Number of NGEs to the right.py
@@ -0,0 +1,6 @@
+# Number of NGEs to the right
+
+# Problem Statement: Given an array arr of size n containing elements, find the next greater element for each element in the array in the order of their appearance.
+# The next greater element of an element in the array is the nearest element on the right that is greater than the current element.
+# If there does not exist a next greater element for the current element, then the next greater element for that element is -1.
+
